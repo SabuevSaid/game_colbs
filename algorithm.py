1b84e824c4d0566b8d14ca1ec7962e3bcbd965de
Kamil <k.sabuev@datamart.ru>
2024-02-22 20:21:03 +0300

    add are_elements_equal

--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,5 @@
-def fun():
-    pass
+def are_elements_equal(value_colors_in_colbs, index1, index2):
+    array1 = value_colors_in_colbs[index1]
+    array2 = value_colors_in_colbs[index2]
+
+    return array1[0][0] == array2[0][0]
